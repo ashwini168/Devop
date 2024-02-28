f94560cf4013fa475ad838ebf53750193d956e94
= <02fe21bcs016@klescet.ac.in>
2024-02-28 16:58:53 +0530

    sprint-1 --class selection -- version 1.0

---
+++ SmartBed.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class SmartBed {
+public:
+    void wakeUp() {
+        std::cout << "Smart bed wakes up the homeowner.\n";}
+};
---
+++ WaterHeater.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class WaterHeater  {
+public:
+    void heatWater() {
+        std::cout << "Water heating...\n";}
+};
